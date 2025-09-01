c777f1d94ed9909a0a84f8a90e50ceec8a4f4675
max quinones <maximilian.quinones@gmail.com>
2025-08-31 21:30:33 -0700

    adding mui material

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,3 +1,4 @@
+import ButtonUsage from '@/components/common/ButtonUsage';
 import React from 'react';
 
 const ServicesPage: React.FC = () => {
@@ -7,7 +8,9 @@ const ServicesPage: React.FC = () => {
         <h1>🏥 Pet Services</h1>
         <p>Professional pet care services coming soon!</p>
         <p>This page will feature grooming, veterinary care, training, and boarding services.</p>
+        <ButtonUsage />
       </div>
+
     </div>
   );
 };
